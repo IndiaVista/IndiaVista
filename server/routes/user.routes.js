ed08ca308b9f084972514854c73307c08fc63433
Sujata-Randhaye <sujatarandhaye@gmail.com>
2024-11-10 19:06:36 +0530

    Feature/Forgot passsword done

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -9,5 +9,5 @@ const router = Router()
 router.route("/register").post(registerUser)
 router.route("/signin").post(loginUser)
 router.route("/change-password").post(verifyJWT,changePassword)
-router.route("/forget-password").post(verifyJWT,resetPasswordToken)
+router.route("/forget-password").post(resetPasswordToken)
 export default router
