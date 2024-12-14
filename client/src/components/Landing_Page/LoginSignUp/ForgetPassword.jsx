84607d0e786f92f58726f1f03612277c936f9f89
Sujata-Randhaye <sujatarandhaye@gmail.com>
2024-12-14 15:31:43 +0530

    Changes done

--- client/src/components/Landing_Page/LoginSignUp/ForgetPassword.jsx
+++ client/src/components/Landing_Page/LoginSignUp/ForgetPassword.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react"
 import { BiArrowBack } from "react-icons/bi"
-import { useDispatch, useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 import { useNavigate,useLocation } from "react-router-dom"
 import { toast } from "react-toastify"
